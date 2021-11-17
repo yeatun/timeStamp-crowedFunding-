81a9999cd64b1568a06d1b9542c1dd728a083911
yeatun <p.yeatun@gmail.com>
2021-11-17 23:26:31 +0600

    changed format

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const { request } = require('express');
 const app = express()
 const port =  4000;
 app.use(express.json())
-
+var givenId;
+var givenTimestamp;
 app.post('/test', (req, res) => {
-  const id = req.body.id
-  const timestamp = req.body.timestamp
-  res.send(`Your  ${id} and ${timestamp} has been posted`)
-  
-
- 
-  
-
-});
-app.get('/test', async (req, res) => {
   const timestamp = req.body.timestamp;
   const id = req.body.id;
   // const time = await unixConverter(timestamp);
   // console.log(time);
-  const timeString = new Date(timestamp);
+  //const timeString = new Date(timestamp);
   const timeStringISO = new Date(timestamp*1000);
+  givenId = id;
+  givenTimestamp = timeStringISO;
   console.log(timeStringISO);
-
-  schedule.scheduleJob(timeStringISO, function(){
-    res.send(`Your id is : ${id} and time is ${timeStringISO}`);
-    console.log(`Your id is : ${id} and time is ${timeStringISO}`);
-    
-
+  res.send(`Your  ${id} and ${timestamp} has been posted`)
+ 
+ 
+});
+app.get('/test', async (req, res) => {
+  console.log(givenTimestamp)
+  const timestamp = givenTimestamp;
+  const id = givenId;
+  schedule.scheduleJob(timestamp, function(){
+    // console.log(`Your id is : ${id} and time is ${timeStringISO}`);
+      res.send(`Your id is : ${id} and time is ${timestamp}`);
+ 
   });
+ 
 })
 app.get('/', (req, res) => {
   res.send('Hello World!')
